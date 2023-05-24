a57420c9e31bdd2fe527ec2082c96fe66e692d39
ridvan-sevimli <68498010+ridvan-sevimli@users.noreply.github.com>
2023-05-24 05:31:18 +0200

    Search view done

--- PageFlip/ContentView.swift
+++ PageFlip/ContentView.swift
@@ -11,37 +11,22 @@ struct ContentView: View {
     @ObservedObject var model = BookModel()
     @State var searchText = ""
     var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass").foregroundColor(.gray)
-            TextField("Search", text: $searchText)
-                                   .foregroundColor(.primary)
-                                   .accentColor(.blue)
-                                   .padding(.horizontal, 10)
-                           }
-                           .padding(.vertical, 10)
-                           .background(Color(.systemGray6))
-                           .cornerRadius(10)
-                           .padding(.horizontal, 10)
-        Button(action:{
-            NavigationStack{
-                if let firstBook = model.data.first{
-                      NavigationLink(destination: DetailView(book: firstBook)){
-                            VStack{
-                                HStack{
-                                    Text(firstBook.title).bold()
-                                }
-                            }
-                        }
-                    }
-            }.task{
-                await model.loadDataAsync()
-            }.refreshable {
-                model.loadData()
+        NavigationStack{
+            HStack {
+                Image(systemName: "magnifyingglass").foregroundColor(.gray)
+                TextField("Search", text: $searchText)
+                                       .foregroundColor(.primary)
+                                       .accentColor(.blue)
+                                       .padding(.horizontal, 10)
+                               }
+                               .padding(.vertical, 10)
+                               .background(Color(.systemGray6))
+                               .cornerRadius(10)
+                               .padding(.horizontal, 10)
+            NavigationLink(destination: DetailViewTest(text: searchText)){
+                Text("Search")
+                
             }
-            
-        }){
-            Text("Search")
-            
         }
     }
 }
---
+++ PageFlip/DetailViewTest.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewTest.swift
+//  PageFlip
+//
+//  Created by ridvan sevimli on 24.05.23.
+//
+
+import SwiftUI
+
+struct DetailViewTest: View {
+    var text : String 
+    var body: some View {
+        Text(text)
+    }
+}
+
+
