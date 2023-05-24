4fa85474e4c4920e121612ea7edc60c6beba71ad
ridvan-sevimli <68498010+ridvan-sevimli@users.noreply.github.com>
2023-05-24 05:05:20 +0200

    Adding Search Field and Search Button

--- PageFlip/ContentView.swift
+++ PageFlip/ContentView.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var model = BookModel()
-    
+    @State var searchText = ""
     var body: some View {
-        NavigationStack{
-            if let firstBook = model.data.first{
-                  NavigationLink(destination: DetailView(book: firstBook)){
-                        VStack{
-                            HStack{
-                                Text(firstBook.title).bold()
+        HStack {
+            Image(systemName: "magnifyingglass").foregroundColor(.gray)
+            TextField("Search", text: $searchText)
+                                   .foregroundColor(.primary)
+                                   .accentColor(.blue)
+                                   .padding(.horizontal, 10)
+                           }
+                           .padding(.vertical, 10)
+                           .background(Color(.systemGray6))
+                           .cornerRadius(10)
+                           .padding(.horizontal, 10)
+        Button(action:{
+            NavigationStack{
+                if let firstBook = model.data.first{
+                      NavigationLink(destination: DetailView(book: firstBook)){
+                            VStack{
+                                HStack{
+                                    Text(firstBook.title).bold()
+                                }
                             }
                         }
                     }
-                }
-        }.task{
-            await model.loadDataAsync()
-        }.refreshable {
-            model.loadData()
+            }.task{
+                await model.loadDataAsync()
+            }.refreshable {
+                model.loadData()
+            }
+            
+        }){
+            Text("Search")
+            
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
